7aafc8a1fd39465648d56e6a52eed423a9a98185
kailashrdave <kailashrdave@users.noreply.github.com>
2018-07-27 11:14:53 +0200

    Add files via upload

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack')
 var libraryName = 'VueShortkey'
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ["babel-polyfill", './src/index.js'],
   output: {
     path: path.resolve(__dirname, '../dist/'),
     publicPath: '../dist/',
